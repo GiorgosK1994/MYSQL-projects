e88f199c26b937a1873f4f92e6826c189b67f8df
GiorgosK1994 <giorgos1994katramidis@gmail.com>
2025-05-14 22:13:01 +0300

    Add files via upload

---
+++ Bike_project.sql
@@ -0,0 +1,9 @@
+-- THIS query shows  each brand top sale without discount
+Select brand_name, product_name, round(sum(oi.list_price),2) AS total_profit_per_product
+FROM order_items oi
+JOIN products pr
+USING (product_id)
+JOIN brands br
+USING (brand_id)
+GROUP BY product_name, brand_name
+order by brand_name, total_profit_per_product DESC
