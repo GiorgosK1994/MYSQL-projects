3e9c9cb297fa3f6551decf0d7ee70b9e56967b34
GiorgosK1994 <giorgos1994katramidis@gmail.com>
2025-05-15 22:56:01 +0300

    Delete Bike_project.sql

--- Bike_project.sql
+++
@@ -1,8 +0,0 @@
-Select brand_name, product_name, round(sum(oi.list_price),2) AS total_profit_per_product
-FROM order_items oi
-JOIN products pr
-USING (product_id)
-JOIN brands br
-USING (brand_id)
-GROUP BY product_name, brand_name
-order by brand_name, total_profit_per_product DESC
